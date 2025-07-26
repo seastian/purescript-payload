613eee5c122f4db87c204e5b888794031069f0b9
seastian <seastian@users.noreply.github.com>
2025-07-26 21:01:51 +0200

    clean imports

--- src/Payload/Server/Guards/HTTP.purs
+++ src/Payload/Server/Guards/HTTP.purs
@@ -2,28 +2,15 @@ module Payload.Server.Guards.HTTP where
 
 import Prelude
 
-import Control.Monad.Except (lift, throwError)
-import Data.Either (Either(..))
 import Data.Map (Map)
-import Data.Symbol (class IsSymbol)
 import Data.Tuple (Tuple)
 import Effect.Aff (Aff)
 import Foreign.Object as Object
-import Node.HTTP as HTTP
 import Node.HTTP.IncomingMessage as IM
 import Payload.HTTP (HTTPRequest)
 import Payload.Headers (Headers)
 import Payload.Headers as Headers
-import Payload.ResponseTypes (Failure(..), Response, Result)
 import Payload.Server.Cookies as Cookies
-import Payload.Server.Internal.GuardParsing (GuardTypes(..))
-import Payload.Server.Response (class EncodeResponse)
-import Payload.Server.Response as Resp
-import Payload.Spec (GCons, GNil, Guards(..), GuardList)
-import Prim.Row as Row
-import Record as Record
-import Type.Equality (to)
-import Type.Proxy (Proxy(..))
 
 -- | Guard for retrieving request headers
 headers :: HTTPRequest -> Aff Headers
